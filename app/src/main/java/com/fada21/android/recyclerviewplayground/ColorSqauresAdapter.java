9554bdc13b4a985df62027d1bf96a965f5e4a122
fada21 <lukasz.czaplicki@gmail.com>
2015-02-16 02:09:15 +0000

    moving to the end when click

--- app/src/main/java/com/fada21/android/recyclerviewplayground/ColorSqauresAdapter.java
+++ app/src/main/java/com/fada21/android/recyclerviewplayground/ColorSqauresAdapter.java
@@ -6,15 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ColorSqauresAdapter extends RecyclerView.Adapter<ColorSqauresAdapter.ViewHolder> {
 
-
     private final List<Integer> data;
 
     public ColorSqauresAdapter(List<Integer> colorList) {
-        data = colorList == null ? Collections.EMPTY_LIST : colorList;
+        data = colorList == null ? Collections.EMPTY_LIST : new LinkedList<>(colorList);
     }
 
     @Override
@@ -24,8 +24,18 @@ public class ColorSqauresAdapter extends RecyclerView.Adapter<ColorSqauresAdapte
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, int position) {
-        viewHolder.getView().setBackgroundColor(data.get(position));
+    public void onBindViewHolder(final ViewHolder viewHolder, int position) {
+        View view = viewHolder.getView();
+        view.setBackgroundColor(data.get(position));
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer curr = data.get(viewHolder.getPosition());
+                data.remove(curr);
+                data.add(curr);
+                notifyItemMoved(viewHolder.getPosition(), getItemCount() - 1);
+            }
+        });
     }
 
     @Override
@@ -46,4 +56,5 @@ public class ColorSqauresAdapter extends RecyclerView.Adapter<ColorSqauresAdapte
             return view;
         }
     }
+
 }
